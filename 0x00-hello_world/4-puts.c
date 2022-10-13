151f26114fd8a7ba9369eda0ec9f2cd64555280e
Alxashe2nd <Alxashe2nd@gmail.com>
2022-10-13 03:16:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+/** main   Printing
+ *
+ * Return: Always  zero (successful)
+ */
+int main(void)
+{
+	puts("\ "Programming is like building a multilingual puzzle");
+	return (0);
+}
